8f5ab1235d35b325a362bdb221415fce06391b18
Timur Akmatov <akmatov_t>
2026-02-02 11:34:55 +0600

    initial commit с redux структурой

---
+++ redux-main/src/features/ui/uiSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+    name: "ui",
+    initialState: {
+        username: "Altay",
+        age: 21,
+        city: "Токмок",
+        theme: "light"
+    },
+
+    reducers: {
+        toggleTheme(state) {
+            state.theme = state.theme === "light" ? "dark": "light"
+        }
+    }
+})
+
+
+export const {  toggleTheme } = uiSlice.actions
+export default uiSlice.reducer
